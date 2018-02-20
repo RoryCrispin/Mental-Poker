1221401462f3c786d7cac50b49628e328f5e6a14
Rory <rozz@MBR.local>
2018-02-20 15:07:17 +0000

    Initial commit

---
+++ Client.py
@@ -0,0 +1,260 @@
+from os import urandom
+import json
+from uuid import uuid4
+
+import redis
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization\
+    import load_pem_parameters, load_pem_public_key
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.serialization import Encoding,\
+    ParameterFormat, PublicFormat
+
+
+class RedisClient():
+    def __init__(self, channel):
+        self.ident = str(uuid4())
+        self.channel = channel
+        self.r = redis.StrictRedis(decode_responses=True)
+        self.p = self.r.pubsub(ignore_subscribe_messages=True)
+        self.p.subscribe(channel)
+        self.queue = []
+
+    def post_message(self, to=None, m_code=None, data=None):
+        return self.r.publish(self.channel,
+                              json.dumps({
+                                  "sender_id": self.ident,
+                                  "to": to,
+                                  "m_code": m_code,
+                                  "data": json.dumps(data)
+                              }))
+
+    def decode_message(message):
+        payload = json.loads(message['data'])
+        payload['data'] = json.loads(payload['data'])
+        return payload
+
+
+class Client(RedisClient):
+    def __init__(self, initial_game):
+        super().__init__('poker_chan')
+        self.game = initial_game(self)
+        self.queue = []
+        self.begin()
+
+    def begin(self):
+        self.post_message(data={'hello': True})
+        for message in self.p.listen():
+            if message['type'] == 'message':
+                payload = RedisClient.decode_message(message)
+
+                if payload.get('sender_id') != self.ident:
+                    # print(payload)
+                    self.queue.append(payload)
+                    self.game, self.queue =\
+                        self.game.apply_queue(self.queue)
+
+
+class GameClient():
+    def __init__(self, cli):
+        self.cli = cli
+
+    # Takes a Queue of messages and returns a new game class along with
+    # a new queue state (With the applied element removed)
+    # These games are still impure in that they can freely send messages to
+    # other clients
+    def apply_queue(self, queue):
+        return (self, queue)
+
+
+class GreetingCli(GameClient):
+    def __init__(self, cli, greetings_sent=0):
+        super().__init__(cli)
+        self.greetings_sent = greetings_sent
+
+    def apply_queue(self, queue):
+        new_queue = []
+        for e in queue:
+            if e.get('data').get('hello'):
+                self.send_greeting(e)
+            else:
+                new_queue.append(e)
+        return (self, new_queue)
+
+    def send_greeting(self, data):
+        self.cli.post_message(data={'Welcome: Player ': self.greetings_sent})
+        self.greetings_sent += 1
+
+
+class DFEKeyExchange(GameClient):
+    def __init__(self, cli):
+        super().__init__(cli)
+        self.dh_server = DHServer()
+        self.send_initiate()
+
+    def apply_queue(self, queue):
+        new_queue = []
+        for e in queue:
+            data = e.get('data')
+            if data.get('dh_begin'):
+                self.send_params()
+                self.send_pubkey()
+
+            elif data.get('dh_step') == 'params':
+                self.handle_params(data)
+                self.send_pubkey()
+
+            elif data.get('dh_step') == 'share_pub':
+                self.recv_pubkey(data)
+
+            elif data.get('type') == 'encrypted_message':
+                self.recv_encrypted_message(data)
+
+            else:
+                new_queue.append(e)
+        return (self, new_queue)
+
+    def send_initiate(self):
+        print("Sending init")
+        self.cli.post_message(data={'dh_begin': True})
+
+    def handle_params(self, e):
+        print("handle params")
+        self.dh_server.parse_parameters(e.get('dh_params'))
+
+    def send_params(self):
+        print("Sending params")
+        params = self.dh_server.get_serialised_parameters()
+        self.cli.post_message(data={'dh_step': 'params', 'dh_params': params})
+
+    def send_pubkey(self):
+        print("Sending pubkey")
+        self.cli.post_message(
+            data={
+                'dh_step':
+                'share_pub',
+                'pub':
+                self.dh_server.get_public_key().public_bytes(
+                    Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode()
+            })
+
+    def recv_pubkey(self, data):
+        peerpub = load_pem_public_key(
+            data['pub'].encode(), backend=default_backend())
+        self.dh_server.submit_public_key(peerpub)
+        print(self.dh_server._shared_key)
+        hkdf = HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=None,
+            info=b'pkr_channel',
+            backend=default_backend())
+        self.dh_server._shared_key = hkdf.derive(self.dh_server._shared_key)
+        message = self.dh_server.encrypt_message("Hello this is secret")
+        print("Secret message send = {}".format(message))
+        self.cli.post_message(data={
+            'type': 'encrypted_message',
+            'message': message
+        })
+
+    def recv_encryted_message(self, data):
+        message = data.get('message')
+        de = self.dh_server.decrypt_message(message)
+        print("Decrypted: {}".format(de))
+
+
+class DHServer(object):
+    def __init__(self):
+        super().__init__()
+        # This example DH server uses a known safe prime, or can generate its
+        # own if the PEM file is not available.
+        # The safe prime used here is ffdhe2048, described here:
+        # https://tools.ietf.org/html/rfc7919#appendix-A.1
+        # There is nothing strictly wrong with generating your own prime,
+        # but this one is well tested.
+
+        import os.path
+        pem_path = os.path.join(os.path.dirname(__file__), 'dh_params.pem')
+        if not os.path.isfile(pem_path):
+            # No PEM file available, generate a new prime of 2048 bits.
+            parameters = dh.generate_parameters(
+                generator=2, key_size=2048, backend=default_backend())
+            s = parameters.parameter_bytes(Encoding.PEM, ParameterFormat.PKCS3)
+            with open('dh_params.pem', 'wb') as outfile:
+                outfile.write(s)
+        # Load PEM file - a standard format for cryptographic
+        # keys and numbers.
+        with open(pem_path, 'r') as f:
+            self.pem_data = f.read().encode("UTF-8")
+        self.parse_parameters(self.pem_data.decode())
+
+    def encrypt_message(self, message):
+        if not self._shared_key:
+            return None
+        message = message.encode()
+        from os import urandom
+        nonce = urandom(16)
+
+        cipher = Cipher(
+            algorithms.AES(self._shared_key),
+            modes.CTR(nonce),
+            backend=default_backend())
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(bytes([b + 12 % 255 for b in message
+                                             ])) + encryptor.finalize()
+        return nonce + ciphertext
+
+    def decrypt_message(self, ciphertext):
+        if self._shared_key is not None:
+            key = self._shared_key
+
+        cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=self.backend)
+        decryptor = cipher.decryptor()
+        plaintext = b''
+        for block in self._get_blocks(ciphertext):
+            plaintext = plaintext + decryptor.update(block)
+        return plaintext + decryptor.finalize()
+
+    def parse_parameters(self, params):
+        # TODO: Use proper library serialisation
+        # https://cryptography.io/en/latest/hazmat/primitives/asymmetric/serialization/#cryptography.hazmat.primitives.serialization.load_pem_parameters
+        bytestring = params.encode()
+        self.parameters = load_pem_parameters(
+            data=bytestring, backend=default_backend())
+
+    # Return DH parameters (prime and generator)
+    def get_serialised_parameters(self):
+        return self.pem_data.decode()
+
+    # Generate a new private key (a random number) and produce the public
+    # key based on this and the parameters.
+    def get_public_key(self):
+        self.privatekey = self.parameters.generate_private_key()
+        self.public_key = self.privatekey.public_key()
+        return self.public_key
+
+    # Receive another public key as part of a handshake, and use it to
+    # calculate a share secret
+    def submit_public_key(self, pk):
+        if pk == None:
+            return
+
+        self._pre_master_secret = self.privatekey.exchange(pk)
+
+        # Derive the key
+        from cryptography.hazmat.primitives import hashes
+        from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+        hkdf = HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=None,
+            info=b'g53sec',
+            backend=default_backend())
+
+        self._shared_key = hkdf.derive(self._pre_master_secret)
+
+
+bob = Client(DFEKeyExchange)
