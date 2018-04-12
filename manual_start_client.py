1cd4023fafdb3b85b7a1ef1521726ddbda1ad8da
Rory Crispin <rorycr@icloud.com>
2018-04-12 14:00:11 +0100

    add manual start client

---
+++ manual_start_client.py
@@ -0,0 +1,19 @@
+import os.path
+import time
+from yaml import dump
+
+from client import CommsClient
+from poker_rounds.poker_sequencer import PokerHandGameSequencer
+
+rounds = PokerHandGameSequencer()
+cli = CommsClient(rounds).begin()
+
+print("~~~~~~~ Game State Log ~~~~~~~~~~")
+print(dump(cli['game'].state_log))
+scriptpath = os.path.dirname(__file__)
+filename = os.path.join(scriptpath, 'states/{}_{}.txt'.format(time.time(), cli['peer_map'][cli['ident']]['roll']))
+
+with open(filename, 'w') as f:
+    f.write(dump(cli))
+    f.close()
+pass
