76ffda7fccf3ecd09bf918928383c84bc152f8fd
Rory Crispin <rorycr@icloud.com>
2018-04-13 14:13:52 +0100

    pretty printing of cards to game state log

---
+++ cardlib/test_card.py
@@ -0,0 +1,11 @@
+from cardlib.Card import Card, Face, Rank
+
+
+def test_init_with_val():
+    card = Card(10)
+    assert repr(card) == "Queen Heart"
+
+
+def test_init_manually():
+    card = Card(Rank.Queen, Face.Heart)
+    assert repr(card) == "Queen Heart"
